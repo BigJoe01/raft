b17e41ecd6f2a914af68ead1f415751ba4c993eb
Constantin S. Pan <kvapen@gmail.com>
2016-07-13 15:55:15 +0300

    Add a python script to run the example servers and clients.

---
+++ example/run-example.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+
+import wtfexpect
+
+try:
+	with wtfexpect.WtfExpect() as we:
+		servers = 'alpha bravo conan'.split()
+		clients = 'xenon yeast zebra'.split()
+		serverids = {name: i for i, name in enumerate(servers)}
+
+		cfg = []
+		baseport = 6000
+		for i in range(len(servers)):
+			cfg.append('-r')
+			cfg.append("%d:%s:%d" % (i, "127.0.0.1", baseport + i))
+
+		for i, s in enumerate(servers):
+			we.spawn(s, [
+				'bin/server',
+				'-i', str(i),
+				*cfg,
+			])
+
+		for c in clients:
+			we.spawn(c, ['bin/client', '-k', c, *cfg])
+
+		while we.alive():
+			timeout = 0.5
+			name, line = we.readline(timeout)
+			if name is None: continue
+
+			if line is None:
+				code = we.getcode(name)
+				if name in servers:
+					print("%d(%s) finished with code %d" % (serverids[name], name, code))
+				else:
+					print("%s finished with code %d" % (name, code))
+			else:
+				if name in servers:
+					print("[%d(%s)] %s" % (serverids[name], name, line))
+				else:
+					print("[%s] %s" % (name, line))
+except KeyboardInterrupt:
+	print("killed")
